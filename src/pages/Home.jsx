42d0a68330a6f976ae84f4c22b852dc0a68e1461
Kid Lei <eric@collectiveequity.com>
2024-03-01 18:47:53 +0000

    added some formatting to the Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Searchform from '../components/Searchform'
 function Home(props) {
     return (
         <div>
+            <br />
             <h1>Dinner Party Planner</h1>
             <div className="container text-center">
                 <p>The website which allows you to compile a series of recipes, for your next dinner party!</p>
