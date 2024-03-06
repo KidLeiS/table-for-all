46c1421dcfcd2599ab6b0f844c5e37f46292aefb
Soo <li_xiuheng@hotmail.com>
2024-03-06 21:57:57 +0000

    delete unused files

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
 import Home from "./pages/Home";
-import History from "./pages/History";
 import About from "./pages/About";
 import "./App.css";
 import Recipe from "./pages/Recipe";
-import SearchResults from "./pages/SearchResults";
 import Searchform from "./components/Searchform";
-import Pages from "./pages/Pages";
 
 function App() {
 	return (
