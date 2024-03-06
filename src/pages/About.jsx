8bde91d52fe43cd2608f18b1c3df16fa9e4d5ac2
Kid Lei <eric@collectiveequity.com>
2024-03-06 19:17:42 +0000

    added the about page and relabled the history page as work in progress

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,26 @@ import React from 'react';
 function About() {
     return (
         <div>
-            <p>About page</p>
+            <h1>About us</h1>
+            <p>This project was created by Soo, Eric and John. We are a band of 3 wannabe web developers taking part in edX's 16 week front end coding bootcamp. We were inspired by our mutual passion for food, and wanted to create an easy way for a home cook to find and compile recipes.</p>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col card text-center">
+                        <h3>Soo</h3>
+                        <p>Hi! I'm Soo</p>
+                    </div>
+                    <div className="col card text-center">
+                        <h3>Eric</h3>
+                        <p>Hi! I'm Eric</p>
+                    </div>
+                    <div className="col card text-center">
+                        <h3>John</h3>
+                        <p>Hi! I'm John</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
